72a092de19ffcf7043a5a2d5cfd604c1938ff556
Gear61 <chessnone@yahoo.com>
2015-11-19 00:10:39 -0800

    Fix about page so orientation change does not gimp it. Fix source thumbnails so it renders right on all devices

--- app/src/main/java/com/randomappsinc/berniesanderssoundboard/Activities/AboutBernieActivity.java
+++ app/src/main/java/com/randomappsinc/berniesanderssoundboard/Activities/AboutBernieActivity.java
@@ -6,23 +6,19 @@ import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.ms.square.android.expandabletextview.ExpandableTextView;
-import com.randomappsinc.berniesanderssoundboard.Adapters.FollowAdapter;
 import com.randomappsinc.berniesanderssoundboard.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnItemClick;
+import butterknife.OnClick;
 
 /**
  * Created by alexanderchiou on 11/18/15.
  */
 public class AboutBernieActivity extends StandardActivity {
     @Bind(R.id.about_bernie) ExpandableTextView aboutBernie;
-    @Bind(R.id.links) ListView links;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +33,30 @@ public class AboutBernieActivity extends StandardActivity {
         Spanned bernieSummaryFull = Html.fromHtml(summary + "<br><br>" + source + feelTheBernLink);
 
         aboutBernie.setText(bernieSummaryFull);
-        links.setAdapter(new FollowAdapter(this));
     }
 
-    @OnItemClick(R.id.links)
-    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-        String followUrl = getResources().getStringArray(R.array.follow_links)[position];
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(followUrl)));
+    @OnClick({R.id.website, R.id.reddit, R.id.facebook, R.id.twitter, R.id.youtube, R.id.google_plus})
+    public void openLink(View view) {
+        String link = null;
+        switch (view.getId()) {
+            case R.id.website:
+                link = getString(R.string.website_link);
+                break;
+            case R.id.reddit:
+                link = getString(R.string.reddit_link);
+                break;
+            case R.id.facebook:
+                link = getString(R.string.facebook_link);
+                break;
+            case R.id.twitter:
+                link = getString(R.string.twitter_link);
+                break;
+            case R.id.youtube:
+                link = getString(R.string.youtube_link);
+                break;
+            case R.id.google_plus:
+                link = getString(R.string.google_plus_link);
+        }
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(link)));
     }
 }
---
+++ app/src/main/java/com/randomappsinc/berniesanderssoundboard/Views/AspectRatioImageView.java
@@ -0,0 +1,29 @@
+package com.randomappsinc.berniesanderssoundboard.Views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by alexanderchiou on 11/18/15.
+ */
+public class AspectRatioImageView extends ImageView {
+    public AspectRatioImageView(Context context) {
+        super(context);
+    }
+
+    public AspectRatioImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AspectRatioImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+        int height = width * getDrawable().getIntrinsicHeight() / getDrawable().getIntrinsicWidth();
+        setMeasuredDimension(width, height);
+    }
+}
