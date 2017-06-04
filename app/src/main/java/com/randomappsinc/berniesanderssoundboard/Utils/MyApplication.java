ece658b57f50b201fe95f661c6986ddbeee3ece1
Gear61 <chessnone@yahoo.com>
2017-06-04 10:58:58 -0700

    Update settings

--- app/src/main/java/com/randomappsinc/berniesanderssoundboard/Utils/MyApplication.java
+++ app/src/main/java/com/randomappsinc/berniesanderssoundboard/Utils/MyApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
+import com.joanzapata.iconify.fonts.IoniconsModule;
 
 import java.io.File;
 
@@ -14,7 +15,8 @@ public final class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Iconify.with(new FontAwesomeModule());
+        Iconify.with(new FontAwesomeModule())
+               .with(new IoniconsModule());
         instance = this;
 
         createExternalDirectory();
